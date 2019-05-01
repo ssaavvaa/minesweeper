bb5450d76028660ec27e8a294e881b5b94fe9778
Anton <toxasava@icloud.com>
2019-05-01 13:45:14 -0400

    Updates

---
+++ precache-manifest.390e28ef2880efd87457e6626b748b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8461d127f44fa15081272a49dd6370",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "1065821a13cd370e9ad7",
+    "url": "/minesweeper/static/css/main.1ece4562.chunk.css"
+  },
+  {
+    "revision": "d78533be795d6f9eee82",
+    "url": "/minesweeper/static/js/2.1d955ed3.chunk.js"
+  },
+  {
+    "revision": "1065821a13cd370e9ad7",
+    "url": "/minesweeper/static/js/main.93cf97fe.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  }
+]);
